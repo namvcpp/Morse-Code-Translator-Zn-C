8e6772d95b6b55cebfd377d5976488bf668a6439
Nam Van <97937953+namvcpp@users.noreply.github.com>
2024-03-19 15:13:32 +0700

    Initialize constant, change code style
    Make the morse and alphabet array constant
    Format the code with better and synchronized code style

--- MORSE_CODE.cpp
+++ MORSE_CODE.cpp
@@ -22,77 +22,55 @@
 
 using namespace std;
 
-string arr_morse_code[60] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "..--..", ".----.", "-.-.--", "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-."};
-string arr_morse_mean[60] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "'", "!", "/", "(", ")", "&", ":", ";", "=", "+", "-", "_", "\"", "$", "@"};
-int n = 54;
+const string arr_morse_code[60] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "..--..", ".----.", "-.-.--", "-..-.", "-.--.", "-.--.-", ".-...", "---...", "-.-.-.", "-...-", ".-.-.", "-....-", "..--.-", ".-..-.", "...-..-", ".--.-."};
+const string arr_morse_mean[60] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "?", "'", "!", "/", "(", ")", "&", ":", ";", "=", "+", "-", "_", "\"", "$", "@"};
+const int n = 54;
 
-string find_morse_mean(string str)
-{
+string find_morse_mean(string &str){
     for(int i = 0 ; i < n ; ++ i)
-    {
         if(str == arr_morse_code[i])
-        {
             return arr_morse_mean[i];
-        }
-    }
     return "#";
 }
 
-string find_morse_code(string str)
-{
+string find_morse_code(string &str){
     for(int i = 0 ; i < n ; ++ i)
-    {
-
         if(str == arr_morse_mean[i])
-        {
             return arr_morse_code[i];
-        }
-    }
     return "#";
 }
 
-string convert_to_morse(string str)
-{
+string convert_to_morse(string &str){
     string res, word;
-    for(int i = 0 ; i < str.size() ; ++ i)
-    {
+    for(int i = 0 ; i < str.size() ; ++ i){
     	string p;
     	p += str[i];
-        if(p == " ")
-        {
+        if(p == " "){
             res += word;
             res += "/ ";
             word = "";
         }
         else
-        {
-            word += find_morse_code(p) + " ";
-        }
+	        word += find_morse_code(p) + " ";
     }
     return res + word;
 }
 
-string convert_to_sentence(string str)
-{
+string convert_to_sentence(string &str){
     string res, letter;
-    for(int i = 0 ; i < str.size() ; ++ i)
-    {
+    for(int i = 0 ; i < str.size() ; ++ i){
     	string p;
     	p += str[i];
-        if(p == " ")
-        {
+        if(p == " "){
             res += find_morse_mean(letter);
             letter = "";
         }
-        else if(p == "/")
-        {
+        else if(p == "/"){
             res += " ";
             ++ i;
         }
         else
-        {
             letter += p;
-        }
     }
     return res + find_morse_mean(letter);
 }
@@ -116,28 +94,20 @@ void instruction(){
 	cout << "\n``````````````````````````````````````````````````````````````````````\n";
 }
 
-string upperCase(string s)
-{
+string upperCase(string &str){
 	string res;
-	for(int i = 0 ; i < s.size() ; ++ i)
-	{
-		if(s[i] >= 'a' && s[i] <= 'z')
-		{
-			res += char(int(s[i]) - 32);
-		}
+	for(int i = 0 ; i < str.size() ; ++ i){
+		if(str[i] >= 'a' && str[i] <= 'z')
+			res += char(int(str[i]) - 32);
 		else
-		{
-			res += s[i];
-		}
+			res += str[i];
 	}
 	return res;
 }
 
-int main()
-{
+int main(){
 	instruction();
-    while(true)
-    {
+    while(true){
     	cout << "Choose one option you want to translate:\n";
 		cout << "(M)orse to sentence\n";
 		cout << "(S)entence to morse\n";
@@ -145,8 +115,7 @@ int main()
     	string option;
     	getline(cin, option);
     	option = upperCase(option);
-    	if(option == "M" || option == "MORSE TO SENTENCE")
-    	{
+    	if(option == "M" || option == "MORSE TO SENTENCE"){
     		cout << "\nPlease enter your Morse code:\n";
     		string s;
     		getline(cin, s);
@@ -154,27 +123,22 @@ int main()
     		cout << "\nSentence:\n" << convert_to_sentence(s) << "\n";
     		cout << "\nDo you want to exit? Press (C)ontinue if you still want to translate, press (E)xit to stop the program\n";
     		string is_exit;
-    		while(true)
-			{
+    		while(true){
 				getline(cin, is_exit);
 				is_exit = upperCase(is_exit);
-	    		if(is_exit == "E" || is_exit == "EXIT")
-	    		{
+	    		if(is_exit == "E" || is_exit == "EXIT"){
 	    			cout << "\nBye, have a good day!";
 	    			return 0;
 				}
-				else if(is_exit == "C" || is_exit == "CONTINUE")
-				{
+				else if(is_exit == "C" || is_exit == "CONTINUE"){
 					cout << "\n``````````````````````````````````````````````````````````````````````\n";
 					break;
 				}
-				else {
+				else
 					cout << "Please enter again!\n";
-				}
 			}
 		}
-		else if(option == "S" || option == "SENTENCE TO MORSE")
-		{
+		else if(option == "S" || option == "SENTENCE TO MORSE"){
 			cout << "\nPlease enter your sentence:\n";
     		string s;
     		getline(cin, s);
@@ -182,27 +146,22 @@ int main()
     		cout << "\nMorse code:\n" << convert_to_morse(s) << "\n";
     		cout << "\nDo you want to exit? Press (C)ontinue if you still want to translate, press (E)xit to stop the program\n";
     		string is_exit;
-    		while(true)
-			{
+    		while(true){
 				getline(cin, is_exit);
 				is_exit = upperCase(is_exit);
-	    		if(is_exit == "E" || is_exit == "EXIT")
-	    		{
+	    		if(is_exit == "E" || is_exit == "EXIT"){
 	    			cout << "\nBye, have a good day!";
 	    			return 0;
 				}
-				else if(is_exit == "C" || is_exit == "CONTINUE")
-				{
+				else if(is_exit == "C" || is_exit == "CONTINUE"){
 					cout << "\n``````````````````````````````````````````````````````````````````````\n";
 					break;
 				}
-				else {
+				else
 					cout << "Please enter again!\n";
-				}
 			}
 		}
-		else
-		{
+		else {
 			cout << "\nThere only two option, please choose again!\n";
 			cout << "\n``````````````````````````````````````````````````````````````````````\n";
 		}
